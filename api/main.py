e94d0d3f94a03b2bf23f4b6456c3fa165be5c768
leafy <raphaelsilvaramalho.com@gmail.com>
2024-12-03 20:08:10 -0300

    Atualizando valores

--- api/main.py
+++ api/main.py
@@ -116,4 +116,4 @@ def deleteSession():
 #--------------------------------------------------------------------
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080, debug=True)
+  app.run(host='0.0.0.0', port=5000, debug=True)
